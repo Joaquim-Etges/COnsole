0133522ec09ad796a91219f13d01cc1bdde8ace7
Joaquim-Etges <140763454+Joaquim-Etges@users.noreply.github.com>
2023-12-21 19:53:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let nome="Joaquim ";
+let sobrenome="de Almeida Etges";
+
+let nomecompleto = nome.concat(sobrenome);
+console.log(nomecompleto);
+
+let pet= nomecompleto.replace ("Joaquim","kiki");
+console.log(pet)
+
+console.log(nomecompleto.length);
+console.log(pet.length);
+
+let a=  Math.floor(Math.random()*101);
+let b=  Math.floor(Math.random()*101);
+
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
